72bd4ad5b00fe61016e638c0a0f2b731ba3947b9
Adriano Rodrigues de Araujo <65834285+AdrianoRAraujo365@users.noreply.github.com>
2026-08-25 09:48:54 -0300

    Create Estilo.js with basic styling for components

---
+++ Estilo.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff'
+  },
+  FontGrande: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  FontMedio: {
+    fontSize: 20,
+    marginBottom: 10
+  }
+});
